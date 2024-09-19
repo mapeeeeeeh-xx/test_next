d85227bd28d65916938661bee579c7514f47a1d1
ferm <fermandrei1@gmail.com>
2024-09-19 10:55:13 +0800

    second try

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,6 +5,7 @@ const DashboardPage = () => {
     <div className="text-black">
       <h1 className="text-3xl font-bold mb-4">Welcome to the Dashboard</h1>
       <p className="text-gray-700">Here is an overview of your activities.</p>
+      {/* TRYYYYY */}
       {/* Add more dashboard content here */}
     </div>
   );
